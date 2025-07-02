9cd5b3a0b06e8f7f71528dba17dc1f5535d6e6ea
docscodes <docscodes@gmail.com>
2025-07-02 15:49:01 -0400

    add users and posts

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
-from typing import Union
-
 from fastapi import FastAPI
+from .routers.posts import router as posts_router
+from .routers.users import router as users_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(posts_router, prefix="/posts", tags=["posts"])
+app.include_router(users_router, prefix="/users", tags=["users"])
